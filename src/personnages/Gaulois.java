75ab8fdb7f5d50c7d620afeaf918ce506ba06b39
Hapex12 <amine.hamdadou@univ-tlse3.fr>
2024-02-15 15:35:41 +0100

    TP1 methode toString

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,38 @@
+package personnages;
+
+public class Gaulois {
+
+	// TODOAuto-generatedmethodstub
+
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
